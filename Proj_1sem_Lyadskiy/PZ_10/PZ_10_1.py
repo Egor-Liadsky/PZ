cbb8309ef9f42cdb3fbc31d65b131ecdc2d6fcac
Egor Lyadskiy <47715067+Egor-Liadsky@users.noreply.github.com>
2022-12-03 17:53:19 +0300

    Create PZ_10_1.py

---
+++ Proj_1sem_Lyadskiy/PZ_10/PZ_10_1.py
@@ -0,0 +1,19 @@
+magistr = {"Лермонтов", "Достоевский", "Маяковский"}
+domKnigi = {"Толстой", "Грибоедов", "Чехов", "Пушкин"}
+bukMarket = {"Пушкин", "Достоевский", "Маяковский"}
+galereya = {"Чехов", "Тютчев", "Пушкин"}
+
+
+if "Пушкин" in magistr and "Тютчев" in magistr:
+        print("magistr")
+
+if "Пушкин" in domKnigi and "Тютчев" in domKnigi:
+        print("domKnigi")
+
+if "Пушкин" in bukMarket and "Тютчев" in bukMarket:
+        print("bukMarket")
+
+if "Пушкин" in galereya and "Тютчев" in galereya:
+        print("galereya")
+
+
