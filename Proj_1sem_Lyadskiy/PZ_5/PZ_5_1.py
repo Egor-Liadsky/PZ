dc696b81887cf1d086fcb3003c502a7740985902
Egor Lyadskiy <47715067+Egor-Liadsky@users.noreply.github.com>
2022-12-08 23:25:19 +0300

    Update PZ_5_1.py

--- Proj_1sem_Lyadskiy/PZ_5/PZ_5_1.py
+++ Proj_1sem_Lyadskiy/PZ_5/PZ_5_1.py
@@ -1,20 +1,25 @@
-def sumInt(num: int) -> int:
-    leftNum = num // 10
-    rightNum = num % 10
-    if len(str(num)) == 3:
-        centerNum = (num % 100)//10
-        return num - (leftNum + rightNum + centerNum)
-    return num - (leftNum + rightNum)
+"""
+Вариант 28
+Составить функцию решения задачи: из заданного числа вычли сумму его цифр. Из
+результата вновь вычли сумму его цифр и т. д. Через сколько таких действий
+получится нуль?
+"""
 
 
-num = int(input())
-count = sumInt(num)
+def take_sum_numeric(number):
+    sum = 0
+    iter_num = 0
+    while abs(iter_num) != len(number):
+        sum += int(number[iter_num])
+        iter_num -= 1
+    return sum
 
-# print(sumInt(num))
 
-while True:
-    print(count)
-    count = sumInt(count)
-    if count == 0:
-        print(0)
-        break
+input_number = int(input('Введите число: '))
+
+acum_step = 0
+while input_number > 0:
+    input_number = input_number - take_sum_numeric(str(input_number))
+    acum_step += 1
+
+print(f"Нуль получится через {acum_step}  действий")
